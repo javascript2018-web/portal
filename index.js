3b66d4e42c2710fd7d1a2bdf2a0e75d462ac1e76
javascript2018-web <fiverrinbox59@gmail.com>
2024-06-08 10:30:14 +0600

    update

--- index.js
+++ index.js
@@ -2,16 +2,11 @@
 const app = require('./app');  // Ensure the correct path and file name
 const database = require('./database/database');
 
-const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 database();
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+
 
 console.log('App Object:', app);  // Check what is actually imported here
 
